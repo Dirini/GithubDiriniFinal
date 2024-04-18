a781def618ebbe237499c41922c30f6326cfeb0c
Dirini <hgu.hi.min@gmail.com>
2024-04-18 20:00:47 +0900

    Initial Commit

---
+++ GithubDiriniFinal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GithubDiriniFinal
+//
+//  Created by 조민 on 4/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GithubDiriniFinal/GithubDiriniFinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubDiriniFinalApp.swift
+//  GithubDiriniFinal
+//
+//  Created by 조민 on 4/18/24.
+//
+
+import SwiftUI
+
+@main
+struct GithubDiriniFinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
